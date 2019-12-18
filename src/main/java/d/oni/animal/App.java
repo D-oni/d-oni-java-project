a87e7f1744644850052c30af37bdbeca46cb120e
leejiwon <wldnjs2516@naver.com>
2019-12-18 16:32:00 +0900

    111출력

--- src/main/java/d/oni/animal/App.java
+++ src/main/java/d/oni/animal/App.java
@@ -4,8 +4,7 @@ package d.oni.animal;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello AnimalSystem");
+        System.out.println("Welcome AnimalSystem");
         System.out.println("111");
-        System.out.println("222");
     }
 }

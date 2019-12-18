874af772ad3b3eb024ef949f5f6862ffff7ab70c
leejiwon <wldnjs2516@naver.com>
2019-12-18 17:17:08 +0900

    .

--- src/main/java/d/oni/animal/App.java
+++ src/main/java/d/oni/animal/App.java
@@ -5,7 +5,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome AnimalSystem");
-       
- 
+        System.out.println("111");
+        System.out.println("222");
     }
 }

a286efb79ad021166fe8f2bf6bc3203573f3f65e
leejiwon <wldnjs2516@naver.com>
2019-12-20 20:46:10 +0900

    변수, 상수, 연산자, 조건문, 반복문, 블록, 배열 등 문법 활용

--- src/main/java/d/oni/animal/StrayAnimal.java
+++ src/main/java/d/oni/animal/StrayAnimal.java
@@ -17,58 +17,173 @@ public class StrayAnimal {
       int viewCount;
 
     }
+    final  int animal_SIZE=100;
+    Animal[] animals = new Animal[animal_SIZE];
+    int animal_count = 0;
 
-    final  int SIZE=100;
-    Animal[] animals = new Animal[SIZE];
+    class Info{
+      int no;
+      String name;
+      int num;
+      String mail;
+      String add;
+      String photo;
+      String phone;
+      String registeredDate;
+      Date date;
+      int viewCount;
 
-    String response;
-    int count = 0;
+    }
+
+    final int info_SIZE=100;
+    Info[] infomations = new Info[info_SIZE];
+    int info_count=0;
+
+    class Board{
+      int num;
+      String text;
+      Date date;
+      int viewCount;
+      int scrap;
+   
+    }
+    final int board_SIZE = 100;
+    Board[] boards = new Board[board_SIZE];
+    int board_count=0;
+
+    String command;
+    do {
+      System.out.println("입력> ");
+      command = keyboard.nextLine();
+
+      switch(command) {
+
+        case "/animal/add":
+          Animal animal = new Animal();
+
+          System.out.println("번호: ");  
+          animal.no=keyboard.nextInt();
+          keyboard.nextLine();
+
+          System.out.println("이름: ");  
+          animal.name=keyboard.nextLine();
+
+          System.out.println("설명: ");  
+          animal.text=keyboard.nextLine();
+
+          System.out.println("1.유기동물 입양  2.유기동물 보호  3.반려동물 찾기 : ");  
+          animal.choose=keyboard.nextInt();
+          keyboard.nextLine();
+
+          System.out.println("동물등록번호여부(Y/n): ");  
+          animal.num=keyboard.nextLine();
+
+          animal.date = new Date(System.currentTimeMillis());
+          animal.viewCount = 0;
+
+          animals[animal_count++]=animal;
+
+          break;
+        case "/animal/list":
+          for (int i = 0; i < animal_count; i++) {
+            Animal a=animals[i];
+            System.out.printf("%d, %s, %s, %d, %s, %s, %d\n", 
+                a.no, a.name, a.text, a.choose, a.num, a.date, a.viewCount);
+          }
+          break;
+        case "/info/add":
+          Info infomation = new Info();
+
+          System.out.println("번호: ");
+          infomation.no = keyboard.nextInt();
+          keyboard.nextLine();
+
+          System.out.println("이름: ");
+          infomation.name=keyboard.nextLine();
+
+          System.out.println("동물등록번호 : ");
+          infomation.num=keyboard.nextInt();
+          keyboard.nextLine();
+
+          System.out.println("이메일: ");
+          infomation.mail=keyboard.nextLine();
+
+          System.out.println("주소 : ");
+          infomation.add=keyboard.nextLine();
+
+          System.out.println("사진 : ");
+          infomation.photo=keyboard.nextLine();
+
+          System.out.println("전화번호 : ");
+          infomation.phone=keyboard.nextLine();
+
+          System.out.println("가입일 : ");
+          infomation.registeredDate=keyboard.nextLine();
+
+          infomation.date = new Date(System.currentTimeMillis());
+
+          infomation.viewCount=0;
+
+          infomations[info_count++]=infomation;
+
+          break;
+        case "/info/list":
+          for(int i=0; i<info_count;i++) { 
+            Info in = infomations[i];
+            System.out.printf("%d,%s,%d,%s,%s,%s,%s,%s,%s,%d\n",
+                in.no, in.name, in.num, in.mail, in.add, in.photo, in.phone, in.registeredDate, in.date, in.viewCount);
+          }
+          break;
+        case "/board/add":
+          Board board = new Board();
+
+          System.out.println("번호: ");
+          board.num=keyboard.nextInt();
+          keyboard.nextLine();
+
+          System.out.println("내용: ");
+          board.text=keyboard.nextLine();
 
 
-    for(int i=0; i<100; i++) {
-      Animal animal = new Animal();
+          System.out.println("스크랩 수: ");
+          board.scrap=keyboard.nextInt();
 
-      System.out.println("번호: ");  
-      animal.no=keyboard.nextInt();
-      keyboard.nextLine();
+          keyboard.nextLine();
 
-      System.out.println("이름: ");  
-      animal.name=keyboard.nextLine();
+          board.date=new Date(System.currentTimeMillis());
 
-      System.out.println("설명: ");  
-      animal.text=keyboard.nextLine();
+          boards[board_count++]=board;
 
-      System.out.println("1.유기동물 입양  2.유기동물 보호  3.반려동물 찾기 : ");  
-      animal.choose=keyboard.nextInt();
-      keyboard.nextLine();
-
-      System.out.println("동물등록번호여부(Y/n): ");  
-      animal.num=keyboard.nextLine();
-
-      animal.date = new Date(System.currentTimeMillis());
-      animal.viewCount = 0;
-
-      animals[i]=animal;
-
-      count++;
-
-      System.out.println();
-
-      System.out.print("계속 입력하시겠습니까?(Y/n) ");
-      response = keyboard.nextLine();
-      if (!response.equalsIgnoreCase("y")) {
-        break;
+          board.viewCount=0;
+        case"/board/list":
+          for(int i=0;i<board_count; i++) {
+            Board b = boards[i];
+            System.out.printf("%d, %s, %d, %s, %d\n",
+                b.num, b.text, b.scrap, b.date, b.viewCount);
+          }
+          break;
+        default:
+          if(!command.equalsIgnoreCase("quit")) {
+            System.out.println("실행할 수 없는 명령입니다.");
+            System.out.println();
+          }
       }
+
+
+    }while(!command.equalsIgnoreCase("quit")); {
+      System.out.println("종료!"); 
+      keyboard.close();
     }
 
-    keyboard.close();
-
-    System.out.println();
-
-    for (int i = 0; i < count; i++) {
-      Animal animal=animals[i];
-      System.out.printf("%d, %s, %s, %d, %s, %s, %d\n", 
-          animal.no, animal.name, animal.text, animal.choose, animal.num, animal.date, animal.viewCount);
-    }
   }
 }
+
+
+
+
+
+
+
+
+
+

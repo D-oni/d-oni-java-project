684f5f7a02a2f7e64102e3636f5de67f14e0c1e2
d-oni <wldnjs2516@naver.com>
2020-03-09 01:50:28 +0900

    .

--- client/src/README.md
+++ client/src/README.md
@@ -1,19 +1,22 @@
-# 간단한 메시지를 서버에 전송하고 응답을 받기
+#  LMS 명령을 입력 받는 기능을 추가하기
 
-- 클라이언트를 서버와 통신 연결할 수 있다.
-- 소켓을 통해 데이터를 입출력 할 수 있다.
+- 사용자로부터 명령을 입력 받을 수 있다.
 
-## 실습 소스 및 결과
+## 소스 및 결과
 
+- src/main/java/d/oni/util 패키지 추가
+- src/main/java/d/oni/util/Prompt.java 추가
+- src/main/java/d/oni/animal/handler 패키지 추가
+- src/main/java/d/oni/animal/handler/Command.java 추가
 - src/main/java/d/oni/animal/ClientApp.java 변경
 
 ## 실습  
 
-### 1. 서버와 연결한다.
+### 1. 31번 프로젝트의 App 클래스에서 명령을 입력 받는 부분을 가져오라.
 
-- ClientApp.java 변경
-
-### 2. 서버에 요청 메시지를 전송하고 응답 메시지를 수신한다.
-
-- ClientApp.java 변경
-
+- d.oni.util 패키지 생성한다.
+- d.oni.util.Prompt 클래스를 가져온다.
+- d.oni.animal.handler 패키지 생성한다.
+- d.oni.animal.handler.Command 인터페이스 가져온다.
+- ClientApp.java 변경한다.
+  - 사용자가 입력한 명령을 처리하는 코드를 가져온다.
--- client/src/main/java/d/oni/animal/ClientApp.java
+++ client/src/main/java/d/oni/animal/ClientApp.java
@@ -1,89 +1,148 @@
-package d.oni.animal;
+// LMS 클라이언트
+package com.eomcs.lms;
 
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
-import java.util.Set;
+import com.eomcs.lms.handler.Command;
+import com.eomcs.util.Prompt;
 
-import d.oni.animal.context.ApplicationContextListener;
-import d.oni.animal.domain.Animal;
-import d.oni.animal.domain.Board;
-import d.oni.animal.domain.Infomation;
-import d.oni.animal.handler.AnimalAddCommand;
-import d.oni.animal.handler.AnimalDeleteCommand;
-import d.oni.animal.handler.AnimalDetailCommand;
-import d.oni.animal.handler.AnimalListCommand;
-import d.oni.animal.handler.AnimalUpdateCommand;
-import d.oni.animal.handler.BoardAddCommand;
-import d.oni.animal.handler.BoardDeleteCommand;
-import d.oni.animal.handler.BoardDetailCommand;
-import d.oni.animal.handler.BoardListCommand;
-import d.oni.animal.handler.BoardUpdateCommand;
-import d.oni.animal.handler.Command;
-import d.oni.animal.handler.InfoAddCommand;
-import d.oni.animal.handler.InfoDeleteCommand;
-import d.oni.animal.handler.InfoDetailCommand;
-import d.oni.animal.handler.InfoListCommand;
-import d.oni.animal.handler.InfoUpdateCommand;
-import d.oni.animal.util.Prompt;
 public class ClientApp {
-	public static void main(String[] args) {
-		System.out.println("클라이언트 수업 관리 시스템");
 
-	    String serverAddr = null;
-	    int port = 0;
+  Scanner keyboard = new Scanner(System.in);
+  Prompt prompt = new Prompt(keyboard);
 
-	    Scanner keyScan = new Scanner(System.in);
+  public void service() {
 
-	    try {
-	      System.out.print("서버? ");
-	      serverAddr = keyScan.nextLine();
+    Deque<String> commandStack = new ArrayDeque<>();
+    Queue<String> commandQueue = new LinkedList<>();
 
-	      System.out.print("포트? ");
-	      port = Integer.parseInt(keyScan.nextLine());
+    HashMap<String, Command> commandMap = new HashMap<>();
+    String command;
 
-	    } catch (Exception e) {
-	      System.out.println("서버 주소 또는 포트 번호가 유효하지 않습니다!");
-	      keyScan.close();
-	      return;
-	    }
+    while (true) {
+      command = prompt.inputString("\n명령> ");
 
-	    try (
-	        Socket socket = new Socket(serverAddr, port);
+      if (command.length() == 0)
+        continue;
 
-	        PrintStream out = new PrintStream(socket.getOutputStream());
-	        Scanner in = new Scanner(socket.getInputStream())) {
+      if (command.equals("quit")) {
+        System.out.println("안녕!");
+        break;
+      } else if (command.equals("history")) {
+        printCommandHistory(commandStack.iterator());
+        continue;
+      } else if (command.equals("history2")) {
+        printCommandHistory(commandQueue.iterator());
+        continue;
+      }
 
-	      System.out.println("서버와 연결되었음!");
+      commandStack.push(command);
 
-	      System.out.print("서버에 보낼 메시지: ");
-	      String sendMsg = keyScan.nextLine();
+      commandQueue.offer(command);
 
-	      out.println(sendMsg);
-	      System.out.println("서버에 메시지를 전송하였음!");
+      Command commandHandler = commandMap.get(command);
 
-	      String message = in.nextLine();
-	      System.out.println("서버로부터 메시지를 수신하였음!");
+      if (commandHandler != null) {
+        try {
+          commandHandler.execute();
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.out.printf("명령어 실행 중 오류 발생: %s\n", e.getMessage());
+        }
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
+      }
+    }
 
-	      System.out.println("서버: " + message);
+    keyboard.close();
 
-	      System.out.println("서버와 연결을 끊었음!");
+  }
 
-	    } catch (Exception e) {
-	      System.out.println("예외 발생:");
-	      e.printStackTrace();
-	    }
+  private void printCommandHistory(Iterator<String> iterator) {
+    int count = 0;
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+      count++;
 
-	    keyScan.close();
+      if ((count % 5) == 0) {
+        String str = prompt.inputString(":");
+        if (str.equalsIgnoreCase("q")) {
+          break;
+        }
+      }
+    }
+  }
 
-	}
+  public static void main(String[] args) throws Exception {
+    System.out.println("클라이언트 수업 관리 시스템입니다.");
+
+    ClientApp app = new ClientApp();
+    app.service();
+
+    /*
+    String serverAddr = null;
+    int port = 0;
+
+    // 키보드 스캐너 준비
+    Scanner keyScan = new Scanner(System.in);
+
+    try {
+      // 사용자로부터 접속할 서버의 주소와 포트 번호를 입력 받는다.
+      System.out.print("서버? ");
+      serverAddr = keyScan.nextLine();
+
+      System.out.print("포트? ");
+      port = Integer.parseInt(keyScan.nextLine());
+
+    } catch (Exception e) {
+      System.out.println("서버 주소 또는 포트 번호가 유효하지 않습니다!");
+      keyScan.close();
+      return;
+    }
+
+    try (
+        // 서버와 연결
+        Socket socket = new Socket(serverAddr, port);
+
+        // 소켓을 통해 데이터를 읽고 쓰는 도구를 준비한다.
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream())) {
+
+      System.out.println("서버와 연결되었음!");
+
+      System.out.print("서버에 보낼 메시지: ");
+      String sendMsg = keyScan.nextLine();
+
+      // 서버에 메시지를 전송한다.
+      // => 서버가 메시지를 받을 때까지 리턴하지 않는다.
+      // => blocking 방식으로 동작한다.
+      out.println(sendMsg);
+      System.out.println("서버에 메시지를 전송하였음!");
+
+      // 서버가 응답한 메시지를 수신한다.
+      // => 서버로부터 한 줄의 메시지를 받을 때까지 리턴하지 않는다.
+      // => blocking 방식으로 동작한다.
+      String message = in.nextLine();
+      System.out.println("서버로부터 메시지를 수신하였음!");
+
+      // 서버가 받은 메시지를 출력한다.
+      System.out.println("서버: " + message);
+
+      System.out.println("서버와 연결을 끊었음!");
+
+    } catch (Exception e) {
+      System.out.println("예외 발생:");
+      e.printStackTrace();
+    }
+
+    keyScan.close();
+    */
+  }
 }
